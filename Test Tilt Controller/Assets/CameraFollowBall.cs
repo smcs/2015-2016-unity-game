4db07477be133fd87178ac78b4e5539c3758c287
Jasen Ripley <jasenripley@Jasens-MacBook-Pro.local>
2016-04-01 11:00:59 -0400

    tilt controls working

---
+++ Test Tilt Controller/Assets/CameraFollowBall.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowBall : MonoBehaviour {
+	public GameObject player;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(player.transform.position.x - 5, player.transform.position.y - 5);
+	}
+}
